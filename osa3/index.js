580ab15021fb2d5482a3e890945199934c452e97
MikkoSandstrom <mikko.sandstrom@hotmail.fi>
2024-01-07 17:37:01 +0200

    uibuild

--- osa3/index.js
+++ osa3/index.js
@@ -7,6 +7,8 @@ app.use(express.json())
 morgan.token('body', (req) => req.method==="POST" ? JSON.stringify(req.body) : ' ')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
+
 let persons = [
     { 
       "name": "Arto Hellas", 
@@ -89,7 +91,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
