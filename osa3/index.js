63cede6181b44b66e7d3b0ee60367befdd732e44
MikkoSandstrom <mikko.sandstrom@hotmail.fi>
2024-01-10 16:39:36 +0200

    uibuild

--- osa3/index.js
+++ osa3/index.js
@@ -11,29 +11,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "name": "Arto Hellas", 
-      "number": "040-123456",
-      "id": 1
-    },
-    { 
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    { 
-      "name": "Dan Abramov", 
-      "number": "12-43-234345",
-      "id": 3
-    },
-    { 
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
-
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
     res.json(persons)
@@ -45,14 +22,13 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req,res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(req.params.id).then(person =>{ 
+      if (person) {
+          res.json(person)
+      } else {
+          res.status(404).end()
+      }
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -77,21 +53,14 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  if (persons.find(person=> person.name===body.name)) {
-    return res.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random()*1000),
-  }
+  })
 
-  persons  = persons.concat(person)
-
-  res.json(person)
+  person.save().then(savedPerson =>{
+    res.json(savedPerson)
+  })
 })
 
 const PORT = process.env.PORT
